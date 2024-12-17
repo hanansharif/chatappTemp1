baf30a4372c90ccac8d8eb13a60001d66ea88a93
Abdul Hanan Sharif <abd.hanangill@gmail.com>
2024-12-17 07:00:33 +0500

    Final Commit

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,27 +1,88 @@
-import React from 'react';
+import React, {
+    useState,
+    useEffect,
+    useLayoutEffect,
+    useCallback
+} from 'react';
+import { TouchableOpacity, Text } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
+import {
+    collection,
+    addDoc,
+    orderBy,
+    query,
+    onSnapshot
+} from 'firebase/firestore';
+import { signOut } from 'firebase/auth';
+import { auth, database } from '../config/firebase';
+import { useNavigation } from '@react-navigation/native';
+import { AntDesign } from '@expo/vector-icons';
+import colors from '../colors';
 
 export default function Chat() {
+    const [messages, setMessages] = useState([]);
+    const navigation = useNavigation();
+
+    const onSignOuy = () => {
+        signOut(auth)
+            //    .then(() => navigation.navigate('Login'))
+            .catch((error) => console.error('Sign out error', error));
+    };
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => (
+                <TouchableOpacity style={{ paddingRight: 10 }} onPress={onSignOuy}>
+                    <AntDesign name="logout" size={24} color={colors.gray} style={{ marginRight: 10 }} />
+                </TouchableOpacity>
+            )
+        });
+    }, [navigation]);
+
+    useLayoutEffect(() => {
+
+        const collectionRef = collection(database, 'chats');
+        const q = query(collectionRef, orderBy('createdAt', 'desc'));
+
+        const unsubscribe = onSnapshot(q, querySnapshot => {
+            console.log('querySnapshot unsusbscribe');
+            setMessages(
+                querySnapshot.docs.map(doc => ({
+                    _id: doc.data()._id,
+                    createdAt: doc.data().createdAt.toDate(),
+                    text: doc.data().text,
+                    user: doc.data().user
+                }))
+            );
+        });
+        return unsubscribe;
+    }, []);
+
+    const onSend = useCallback((messages = []) => {
+        setMessages(previousMessages =>
+            GiftedChat.append(previousMessages, messages)
+        );
+        // setMessages([...messages, ...messages]);
+        const { _id, createdAt, text, user } = messages[0];
+        addDoc(collection(database, 'chats'), {
+            _id,
+            createdAt,
+            text,
+            user
+        });
+    }, []);
+
     return (
         <GiftedChat
-        // messages={[
-        //     {
-        //         _id: 1,
-        //         text: 'Hello, how are you?',
-        //         createdAt: new Date(),
-        //         user: {
-        //             _id: 2,
-        //             name: 'User',
-        //             avatar: 'https://placeimg.com/140/140/any',
-        //         },
-        //     },
-        // ]}
-        // onSend={messages => console.log(messages)}
-        // user={{
-        //     _id: 1,
-        //     name: 'User',
-        //     avatar: 'https://placeimg.com/140/140/any',
-        // }}
+            messages={messages}
+            onSend={messages => onSend(messages)}
+            user={{
+                _id: auth?.currentUser?.email,
+                avatar: 'https://i.pravatar.ccc/300'
+            }}
+            messagesContainerStyle={{
+                backgroundColor: '#fff'
+            }}
         />
     )
 }
